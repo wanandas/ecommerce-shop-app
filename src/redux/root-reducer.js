cfa2bf0d17255be12683a728eeb679f79ca90310
unknown <wanzerox@gmail.com>
2019-12-13 01:34:22 +0700

    moved our directory and shop data into its own reducer, created respective and updated corresponding components with new ux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,8 @@ import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directotyReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 
 
 const persistConfig = {
@@ -14,7 +16,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directotyReducer,
+    shop: shopReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
