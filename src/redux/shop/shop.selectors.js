8d40a987940d6e77d46497f8f0c197a0f7acd430
unknown <wanzerox@gmail.com>
2020-01-08 03:15:29 +0700

    Hoc with-spinner

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,19 +1,19 @@
-import { createSelector } from 'reselect'
+import { createSelector } from "reselect";
 
-const selectShop = state => state.shop
+const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
-)
+  [selectShop],
+  shop => shop.collections
+);
 
 export const selectCollectionsForPreview = createSelector(
-    [selectShopCollections],
-    collections => Object.keys(collections).map(key => collections[key])
-)
+  [selectShopCollections],
+  collections =>
+    collections ? Object.keys(collections).map(key => collections[key]) : []
+);
 
-export const selectCollection = collectionUrlParam => 
-    createSelector(
-        [selectShopCollections],
-        collections => collections[collectionUrlParam]
-    )
+export const selectCollection = collectionUrlParam =>
+  createSelector([selectShopCollections], collections =>
+    collections ? collections[collectionUrlParam] : null
+  );
