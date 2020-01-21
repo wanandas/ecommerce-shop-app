5374f606bed7667d85005302dd869b074fcb0638
unknown <wanzerox@gmail.com>
2020-01-21 19:37:07 +0700

    create saga for sign in after sign up success

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -35,3 +35,18 @@ export const signOutFailure = err => ({
   type: UserActionTypes.SIGN_OUT_FAILURE,
   payload: err
 });
+
+export const signUpStart = userCredentials => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: userCredentials
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+  type: UserActionTypes.SIGN_UP_SUCCESS,
+  payload: { user, additionalData }
+});
+
+export const signUpFailure = err => ({
+  type: UserActionTypes.SIGN_UP_FAILURE,
+  payload: err
+});
